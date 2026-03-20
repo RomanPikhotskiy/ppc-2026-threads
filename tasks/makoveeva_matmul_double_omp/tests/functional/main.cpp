5df19913bdefe8ee9aed386d2e149993887455c4
sofaprog <sofia_makoveeva@mail.ru>
2026-03-20 23:58:28 +0300

    tidy3

--- tasks/makoveeva_matmul_double_omp/tests/functional/main.cpp
+++ tasks/makoveeva_matmul_double_omp/tests/functional/main.cpp
@@ -10,44 +10,43 @@
 #include "makoveeva_matmul_double_omp/common/include/common.hpp"
 #include "makoveeva_matmul_double_omp/omp/include/ops_omp.hpp"
 #include "util/include/func_test_util.hpp"
+#include "util/include/util.hpp"
 
 namespace makoveeva_matmul_double_omp {
 
-class MatmulDoubleOMPFuncTest : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+class MakoveevaRunFuncTestsThreads : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
-    return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param) + "_omp";
+    size_t n = std::get<0>(test_param);
+    std::string desc = std::get<1>(test_param);
+    return desc + "_" + std::to_string(n) + "x" + std::to_string(n);
   }
 
  protected:
   void SetUp() override {
-    // Правильный способ получить TestType из GetParam()
-    const auto &gtest_param = GetParam();
-    // gtest_param - это tuple<TestType, string, ...>
-    const TestType &params = std::get<0>(gtest_param);
+    TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
 
-    const size_t n = std::get<0>(params);
-    const size_t size = n * n;
+    size_t n = std::get<0>(params);
+    size_t size = n * n;
+
+    const double start = 0.5;
+    const double step = 0.75;
 
     std::vector<double> a(size);
     std::vector<double> b(size);
 
     for (size_t i = 0; i < size; ++i) {
-      a[i] = static_cast<double>(i + 1);
-      b[i] = static_cast<double>(size - i);
+      a[i] = start + (static_cast<double>(i) * step);
+    }
+
+    for (size_t i = 0; i < size; ++i) {
+      b[i] = start + (static_cast<double>(size - 1 - i) * step);
     }
 
     input_data_ = std::make_tuple(n, a, b);
 
     std::vector<double> expected(size, 0.0);
-    for (size_t i = 0; i < n; ++i) {
-      for (size_t k = 0; k < n; ++k) {
-        const double aik = a[(i * n) + k];
-        for (size_t j = 0; j < n; ++j) {
-          expected[(i * n) + j] += aik * b[(k * n) + j];
-        }
-      }
-    }
+    ReferenceMultiply(a, b, expected, n);
     expected_output_ = expected;
   }
 
@@ -69,6 +68,19 @@ class MatmulDoubleOMPFuncTest : public ppc::util::BaseRunFuncTests<InType, OutTy
     return input_data_;
   }
 
+  static void ReferenceMultiply(const std::vector<double> &a, const std::vector<double> &b, std::vector<double> &c,
+                                size_t n) {
+    for (size_t i = 0; i < n; ++i) {
+      for (size_t j = 0; j < n; ++j) {
+        double sum = 0.0;
+        for (size_t k = 0; k < n; ++k) {
+          sum += a[(i * n) + k] * b[(k * n) + j];
+        }
+        c[(i * n) + j] = sum;
+      }
+    }
+  }
+
  private:
   InType input_data_;
   OutType expected_output_;
@@ -76,23 +88,24 @@ class MatmulDoubleOMPFuncTest : public ppc::util::BaseRunFuncTests<InType, OutTy
 
 namespace {
 
-TEST_P(MatmulDoubleOMPFuncTest, FoxMatmul) {
+TEST_P(MakoveevaRunFuncTestsThreads, MatMulFoxAlg) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 10> kTestParams = {
-    std::make_tuple(1, "1"), std::make_tuple(2, "2"),  std::make_tuple(3, "3"), std::make_tuple(4, "4"),
-    std::make_tuple(5, "5"), std::make_tuple(6, "6"),  std::make_tuple(7, "7"), std::make_tuple(8, "8"),
-    std::make_tuple(9, "9"), std::make_tuple(10, "10")};
+const std::array<TestType, 12> kTestParams = {
+    std::make_tuple(1, "size_1x1"),    std::make_tuple(2, "size_2x2"),    std::make_tuple(3, "size_3x3"),
+    std::make_tuple(4, "size_4x4"),    std::make_tuple(5, "size_5x5"),    std::make_tuple(6, "size_6x6"),
+    std::make_tuple(7, "size_7x7"),    std::make_tuple(8, "size_8x8"),    std::make_tuple(9, "size_9x9"),
+    std::make_tuple(10, "size_10x10"), std::make_tuple(16, "size_16x16"), std::make_tuple(32, "size_32x32")};
 
 const auto kTestTasksList =
     ppc::util::AddFuncTask<MatmulDoubleOMPTask, InType>(kTestParams, PPC_SETTINGS_makoveeva_matmul_double_omp);
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
-const auto kTestName = MatmulDoubleOMPFuncTest::PrintFuncTestName<MatmulDoubleOMPFuncTest>;
+const auto kPerfTestName = MakoveevaRunFuncTestsThreads::PrintFuncTestName<MakoveevaRunFuncTestsThreads>;
 
-INSTANTIATE_TEST_SUITE_P(MatmulOMPTests, MatmulDoubleOMPFuncTest, kGtestValues, kTestName);
+INSTANTIATE_TEST_SUITE_P(MatMulFoxAlg_ArithmeticProgression, MakoveevaRunFuncTestsThreads, kGtestValues, kPerfTestName);
 
 }  // namespace
 
